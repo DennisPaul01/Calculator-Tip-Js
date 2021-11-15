5addbee5939190e2e829ecb310dde57d18340960
DennisPaul01 <denys_paul08@yahoo.com>
2021-11-15 20:14:23 +0200

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//DOM
+const inputBill = document.querySelector(".introduce-bill__input");
+const inputCustomTip = document.querySelector(".tip__custom");
+
+inputBill.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    console.log("Deni");
+  }
+});
